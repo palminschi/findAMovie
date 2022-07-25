5122ef302433a104ab506b423ad270c77e45bebc
palminschi <mihaipalminschi@gmail.com>
2022-07-25 20:37:26 +0200

    Firebase added

--- app/src/main/java/com/palmdev/findamovie/App.kt
+++ app/src/main/java/com/palmdev/findamovie/App.kt
@@ -1,6 +1,10 @@
 package com.palmdev.findamovie
 
 import android.app.Application
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 import com.palmdev.findamovie.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -22,6 +26,18 @@ class App : Application() {
                 )
             )
         }
+
+        // Firebase init
+        FirebaseApp.initializeApp(this)
+        // Firebase Messaging init
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task: Task<String> ->
+                if (!task.isSuccessful) {
+                    return@addOnCompleteListener
+                }
+                val token = task.result
+                Log.d("TAG", "Token ->$token")
+            }
 
     }
 }
